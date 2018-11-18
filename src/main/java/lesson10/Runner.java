8f4e42386c240752832561fac7ec41fedbab206a
victor <viktorlsn@gmail.com>
2018-11-18 21:55:29 +0200

    refactor

--- src/main/java/lesson10/Runner.java
+++ src/main/java/lesson10/Runner.java
@@ -2,16 +2,6 @@ package lesson10;
 
 public class Runner {
 
-
-//    добавление элемента
-//    удаление
-//            поиск
-//    вывод всех записей с сортировкой по указанному полю
-//    редактирование
-//    запись в файл (по желанию)
-//    загрузка из файла (по желанию).
-
-
     public static void main(String[] args) {
        NavigateConsolePanel panel = new NavigateConsolePanel();
         panel.readConsole();
